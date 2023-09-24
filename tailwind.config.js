2be16c2974ba8bd4bf2d8f4176b0c98e96bbd38d
Matteo Kinkela <mkinkela123@gmail.com>
2023-09-24 15:57:50 +0200

    Add design for scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import TailwindScrollbar from "tailwind-scrollbar";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -16,5 +18,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [TailwindScrollbar({ nocompatible: true })],
 };
